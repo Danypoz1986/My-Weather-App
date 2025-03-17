8462e290fe9d8bafd933bd903af878701a9ff5b2
Daniel Pozzoli <daniel.pozzoli86@gmail.com>
2025-03-17 17:43:06 +0200

    Trying to solve .env issues

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,16 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly APP_WEATHER_API_KEY: string
-    // more env variables...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
+  readonly VITE_WEATHER_API_KEY: string;
+  readonly VITE_APP_FIREBASE_API_KEY: string;
+  readonly VITE_APP_AUTH_DOMAIN: string;
+  readonly VITE_APP_PROJECT_ID: string;
+  readonly VITE_APP_STORAGE_BUCKET: string;
+  readonly VITE_APP_MESSAGING_SENDER_ID: string;
+  readonly VITE_APP_APP_ID: string;
+  readonly VITE_APP_MEASUREMENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), 'VITE_'); // Ensure only VITE_ prefixed variables are loaded
 
   return {
     plugins: [react()],
-    envPrefix: 'VITE_',
     define: {
-      'import.meta.env': env,
+      'import.meta.env': JSON.stringify(env), // Ensure env variables are stringified
     },
   };
 });
