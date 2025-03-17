0ccbf77351f0b2bd66615ad1078053dc77d9345f
Daniel Pozzoli <daniel.pozzoli86@gmail.com>
2025-03-17 23:05:07 +0200

    chunkSizeWarningLimit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_'); // Ensure only VITE_ prefixed variables are loaded
 
   return {
+    build: {
+      chunkSizeWarningLimit: 1000 // Set limit to 1000KB
+    },
     plugins: [react()],
     define: {
       'import.meta.env': JSON.stringify(env), // Ensure env variables are stringified
